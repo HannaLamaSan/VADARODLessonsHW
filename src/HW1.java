a7afa91bc282e3b5c94ae5de5cb0b5c14e667db0
anna.beganskaya <abeganskaya98@gmail.com>
2025-02-11 19:18:06 +0300

    First commit

---
+++ src/HW1.java
@@ -0,0 +1,6 @@
+public class HW1 {
+    public static void HW() {
+        System.out.println("Беганская Анна Сергеевна. Задание получено 06.02.2025 в 23:04");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        HW1 myObject = new HW1();
+        HW1.HW();
+    }
+}
