95f7f79f28266527aaa61a9868ee8bae464ec8f8
anna.beganskaya <abeganskaya98@gmail.com>
2025-02-23 09:59:40 +0300

    HW4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 import HW1.HW1;
 import HW3.HW3;
+import HW4.HW4;
 
 public class Main {
     public static void main(String[] args) {
 
         //HW1.HW();
-        HW3.taskOne();
-        HW3.taskTwo();
-        HW3.taskThree();
-        HW3.taskFour();
+        //HW3.taskOne();
+        //HW3.taskTwo();
+        //HW3.taskThree();
+        //HW3.taskFour();
+        HW4.leson4Task1();
+        HW4.leson4Task2();
+        HW4.leson4Task3();
+
     }
 }
